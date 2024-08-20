1ff6bb61bc764c6fd31c6adc8701d15ff17b469c
monoronjon sarker <monoronjons86@gmail.com>
2024-08-20 22:08:18 +0600

    footer-add

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+
+const Footer = () => {
+    return (
+        <div className="grid grid-cols-4 mx-10 my-5">
+            <div>
+                    <h1>Contact us</h1>
+                    <h1>ADDRESS:</h1>
+                    <p>Road: 4, House: 15, Dhanmondi R/A,</p>
+                    <p>Dhaka-1205, Bangladesh.</p>
+            </div>
+            <div>
+                <h1>The Company</h1>
+                <h1>About Ecowoods</h1>
+                <p>Privacy Policy</p>
+                <p>Return Policy</p>
+                <p>Ecowoods Policies</p>
+            </div>
+            <div>
+                <h1>Need Help?</h1>
+                <h1>Contact Us</h1>
+                <h1>FAQ</h1>
+                <p>Showroom Locator</p>
+                <p>Billing Terms & Conditions</p>
+            </div>
+            <div>
+                <h1>More Information</h1>
+                <h1>Company Profile</h1>
+                <p>Catalogues</p>
+                <p>Our Team</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
