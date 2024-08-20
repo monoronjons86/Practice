453267ea213f3c9360b627be5636062a1fbbfcb0
monoronjon sarker <monoronjons86@gmail.com>
2024-08-20 10:36:52 +0600

    json length add

--- src/Component/Products/Products.jsx
+++ src/Component/Products/Products.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
 
 const Products = () => {
-    const [users,setUsers]=useState([])
-    useEffect(()=>{
-        fetch('https://dummyjson.com/products')
-        .then(res=>res.json())
-         .then(data=>setUsers(data))  
-
-    },[])
-    return (
-        <div>
-            <h1>users:{users.length}</h1>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+ 
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        console.log(data.products)
+      });
+  }, []);
+  return (
+    <div>
+      <h1>products:{products.length}</h1>
+      {
+        products.map((product)=> <h1 key={product.id}>{product.title}</h1> )
+      }
+    </div>
+  );
 };
 
 export default Products;
