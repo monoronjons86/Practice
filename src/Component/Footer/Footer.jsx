62ba8f1fc53ed1a8ccdf6ec30ceb46ab5bd3b970
monoronjon sarker <monoronjons86@gmail.com>
2024-08-22 17:36:13 +0600

    mmm

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,34 +1,78 @@
 
 const Footer = () => {
     return (
-        <div className="grid grid-cols-4 mx-10 my-5">
-            <div>
-                    <h1>Contact us</h1>
-                    <h1>ADDRESS:</h1>
-                    <p>Road: 4, House: 15, Dhanmondi R/A,</p>
-                    <p>Dhaka-1205, Bangladesh.</p>
+        <footer className="bg-gray-500 p-16">
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-6">
+                <div>
+                    <div className="flex items-center gap-2 pb-6">
+                        <img src="./image/codeabc_circle.png" alt=""/>
+                        <h1 className="font-bold">Code ABC</h1>
+                    </div>
+                    <div>
+                        <p>Terms of use | Privacry</p>
+                        <p>Terms of use | Privacry</p>
+                        
+                    </div>
+
+                </div>
+                <div >
+                    <div className="pb-6">
+                        <h1 className="font-bold">About us</h1>
+                    </div>
+                    <div>
+                        <p>Support center</p>
+                        <p>Customer Support </p>
+                        <p>About</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="pb-6">
+                        <h1 className="font-bold">Our information</h1>
+                    </div>
+                    <div>
+                        <p>Support center</p>
+                        <p>Customer Support </p>
+                        <p>About</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="pb-6">
+                        <h1 className="font-bold">My Account</h1>
+                    </div>
+                    <div>
+                        <p>Support center</p>
+                        <p>Customer Support </p>
+                        <p>About</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="pb-6">
+                        <h1 className="font-bold">Connect</h1>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-2">
+                            <img src="./image/facebook.png" alt=""/>
+                            <p>Facebook</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <img src="./image/twitter.png" alt=""/>
+                            <p>Twitter</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <img src="./image/github.png" alt=""/>
+                            <p>Github</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <img src="./image/dribble.png" alt=""/>
+                            <p>Dribble</p>
+                        </div>
+                
+                    </div>
+                </div>
+
             </div>
-            <div>
-                <h1>The Company</h1>
-                <h1>About Ecowoods</h1>
-                <p>Privacy Policy</p>
-                <p>Return Policy</p>
-                <p>Ecowoods Policies</p>
-            </div>
-            <div>
-                <h1>Need Help?</h1>
-                <h1>Contact Us</h1>
-                <h1>FAQ</h1>
-                <p>Showroom Locator</p>
-                <p>Billing Terms & Conditions</p>
-            </div>
-            <div>
-                <h1>More Information</h1>
-                <h1>Company Profile</h1>
-                <p>Catalogues</p>
-                <p>Our Team</p>
-            </div>
-        </div>
+         </footer>
+
     );
 };
 
--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/prop-types */
 
 const Product = ({product}) => {
+    // eslint-disable-next-line no-unused-vars
     const {title, description,brand,category,images} =product
     return (
-        <div className="border p-6 ">
-                <img src={images} alt="" />
-                <h1>title:{title}</h1>
-                <h1>brand:{brand}</h1>
-                <h2>Category:{category}</h2>
-                <p>description:{description}</p>
-                <div className="text-center my-5">
-                <button className=" bg-lime-500 px-5 py-3" >Add</button>
-                </div>
-            </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img src={images} alt="" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <p className="text-gray-700 text-base">{description}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2 text-center">
+          <button className="bg-lime-600 px-3 py-2 rounded-lg">Add me</button>
+        </div>
+      </div>
  
     );
 };
